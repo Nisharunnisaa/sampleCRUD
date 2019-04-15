e980a42c63a3b61f1ff9f22633266a50f3cfbada
Nisharunnisaa <38999595+Nisharunnisaa@users.noreply.github.com>
2019-04-15 18:16:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,53 +1,9 @@
+var http = require('http');
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-//const mime = require('mime');
-const { Storage } = require('@google-cloud/storage');
-var formidable = require('formidable');
-
-const keyFilename="./uploadfilestonode-firebase-adminsdk-vifhf-80404ec328.json"; //replace this with api key file
-const projectId = "uploadfilestonode" //replace with your project id
-const bucketName = "uploadfilestonode.appspot.com";
-
-
-const storage = new Storage({
-    projectId,
-    keyFilename
-});
-
-const bucket = storage.bucket(bucketName);
-
-/* <input type="file" [formControl]="profImg"/> */
-
-const filePath = './mountains.jpg';
-const uploadTo = '' +filePath;
-//const fileMime = mime.lookup(filePath);
-
-
-bucket.upload(filePath,{
-    destination:uploadTo,
-    public:true,
-    metadata: {contentType: 'image/jpeg',cacheControl: "public, max-age=300"}
-}, function(err, file) {
-    if(err)
-    {
-        console.log(err);
-        return;
-    }
-    console.log(createPublicFileURL(uploadTo));
-});
-
-
-function createPublicFileURL(storageName) {
-    return `http://storage.googleapis.com/${bucketName}/${encodeURIComponent(storageName)}`;
-
-}
-
-app.listen(3000, function() {
-console.log("Server running 3000");
-});
-
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(8080);
 
 
 
